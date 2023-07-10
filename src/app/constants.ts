501851791e7d5974c6157a1854cb553343b7f109
Harry Dulaney <harrydulaney@gmail.com>
2023-07-09 23:01:30 -0400

    contact link

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -136,7 +136,7 @@ export const API_ROUTES = {
 }
 
 export const PROJECT_LINKS = {
-  CONTACT: 'https://harrydulaney.com/contact',
+  CONTACT: 'https://harrydulaney.com',
   ABOUT: 'https://github.com/HarryDulaney/pfolie#readme',
   SOURCE_CODE: 'https://github.com/HarryDulaney/pfolie',
   ISSUES: 'https://github.com/HarryDulaney/pfolie/issues'
